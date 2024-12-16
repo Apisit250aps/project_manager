4359d09234c5ae83f33dc38fb6bdc0d8b2d1bf46
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-12-16 13:32:45 +0700

    auth middleware

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+
+import { NextResponse, type NextRequest } from "next/server";
+
+
+export default async function middleware(req:NextRequest){
+
+    const token = req.cookies.get("authjs.session-token")?.value
+   const { pathname } = req.nextUrl
+
+   // ถ้าไม่มี token และพยายามเข้า dashboard 
+   if (!token && pathname.startsWith('/dashboard')) {
+       return NextResponse.redirect(new URL('/auth/login', req.url))
+   }
+
+   // ถ้ามี token แล้วพยายามเข้า auth routes
+   if (token && pathname.startsWith('/auth')) {
+       return NextResponse.redirect(new URL('/dashboard', req.url))
+   }
+
+   return NextResponse.next()
+ 
+}
+
+export const config = {
+    matcher: [
+        '/dashboard/:path*',
+        '/auth/login',
+        '/auth/register'
+    ]
+ }
